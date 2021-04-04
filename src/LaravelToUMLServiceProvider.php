d7feef055c8b71aedaa3b3d77188dafaebe7dbb2
Andy Abi Haidar <andy@yllw.com>
2021-04-04 23:57:35 +0300

    Added config publishing

--- src/LaravelToUMLServiceProvider.php
+++ src/LaravelToUMLServiceProvider.php
@@ -16,5 +16,11 @@ class LaravelToUMLServiceProvider extends ServiceProvider {
     public function boot() {
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'laravel-to-uml');
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/laravel-to-uml.php' => config_path('laravel-to-uml.php'),
+            ], 'config');
+        }
     }
 }
